0be9a60e7da44d98ca599a4cf98a8fdb7c238c88
daanh432 <daan@daanhendriks.nl>
2022-06-03 09:53:30 +0200

    Simple packaging options

--- Model/Repacking/BinTree.fs
+++ Model/Repacking/BinTree.fs
@@ -11,9 +11,11 @@ type BinTree =
     | Bin of BinIdentifier * List<BinTree>
     /// A product is represented by its part number.
     | Product of PartNumber
+    | PackagedProduct of PartNumber
 
 /// Determines how many products are contained in all bins of the given bin tree.
 let rec productCount binTree =
     match binTree with
     | Bin (_, productsOrBins) -> List.sumBy productCount productsOrBins
     | Product _ -> 1
+    | PackagedProduct _ -> 1
